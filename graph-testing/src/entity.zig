34680eba7bd9c30009795c83a05b183720682964
Øyvind Nordsveen <oyvind.nordsveen@forsta.com>
2025-11-21 14:09:51 +0100

    -

--- graph-testing/src/entity.zig
+++ graph-testing/src/entity.zig
@@ -16,15 +16,16 @@ const ConstructorData = struct {
     progress: f32,
     output: ArrayList(Item),
 };
-const UiData = struct { rectangle: rl.Rectangle };
+pub const UiData = struct { rectangle: rl.Rectangle };
 pub const Entity = struct {
     id: usize,
     kind: EntityKind,
     uiData: ?UiData,
-    pub fn draw(self: Entity) void {
+    allocator: std.mem.Allocator,
+    pub fn draw(self: Entity) !void {
         switch (self.kind) {
             EntityKind.Constructor => {
-                DrawConstructor(self);
+                try DrawConstructor(self);
             },
         }
     }
@@ -37,8 +38,8 @@ pub const Entity = struct {
         }
     }
 
-    pub fn init(id: usize, kind: EntityKind) !Entity {
-        return Entity{ .id = id, .kind = kind, .uiData = null };
+    pub fn init(allocator: std.mem.Allocator, id: usize, kind: EntityKind, uiData: ?UiData) !Entity {
+        return Entity{ .allocator = allocator, .id = id, .kind = kind, .uiData = uiData orelse null };
     }
     pub fn format(
         self: Entity,
@@ -53,10 +54,38 @@ pub fn CreateConstructor(allocator: std.mem.Allocator, res: *Recipe) !EntityKind
     return constData;
 }
 
-fn DrawConstructor(e: Entity) void {
+fn DrawConstructor(e: Entity) !void {
     if (e.uiData) |data| {
-        rl.DrawRectangleRounded(data.rectangle, 0.2, 10, rl.BLUE);
+        const xStartPos: c_int = @intFromFloat(data.rectangle.x);
+        const yStartPos: c_int = @intFromFloat(data.rectangle.y);
+        rl.DrawRectangleRounded(data.rectangle, 0.2, 10, rl.WHITE);
+        rl.DrawRectangleRoundedLines(data.rectangle, 0.2, 10, rl.PURPLE);
+        rl.DrawRectangleRounded(.{ .width = data.rectangle.width, .x = data.rectangle.x, .y = data.rectangle.y, .height = 20 }, 1, 10, rl.SKYBLUE);
+        rl.DrawText("Constructor", xStartPos + 20, yStartPos + 5, 10, rl.WHITE);
+
+        const entityData = &e.kind.Constructor;
+        rl.DrawText("Recipe: ", xStartPos + 20, yStartPos + 20, 10, rl.BLACK);
+        if (entityData.recipe) |recipe| {
+            rl.DrawText(@ptrCast(recipe.name.ptr), xStartPos + 60, yStartPos + 20, 10, rl.BLACK);
+        }
+        rl.DrawText("Progress: ", xStartPos + 20, yStartPos + 40, 10, rl.BLACK);
+        // rl.DrawText(@intFromFloat(entityData.progress),  @intFromFloat(data.rectangle.x + 20), @intFromFloat(data.rectangle.y+85), 10, rl.WHITE);
+        DrawProgressBar(xStartPos + 20, yStartPos + 50, data.rectangle.width - 60, entityData.progress);
+        rl.DrawText("Output: ", xStartPos + 20, yStartPos + 60, 10, rl.BLACK);
+        if (entityData.output.items.len > 0) {
+            var buffer: [20]u8 = undefined;
+            const result = try std.fmt.bufPrintZ(&buffer, "{}", .{entityData.output.items.len});
+            // result[result.len] = 0;
+            rl.DrawText(@ptrCast(result), @intFromFloat(data.rectangle.x + 60), @intFromFloat(data.rectangle.y + 60), 10, rl.BLACK);
+            const item = &entityData.output.getLast();
+            rl.DrawText(@ptrCast(item.name), @intFromFloat(data.rectangle.x + 70), @intFromFloat(data.rectangle.y + 60), 10, rl.BLACK);
+        }
     }
+}
+
+fn DrawProgressBar(xPos: c_int, yPos: c_int, width: f32, progress: f32) void {
+    rl.DrawRectangleLines(xPos, yPos, @intFromFloat(width), 5, rl.PURPLE);
+    rl.DrawRectangle(xPos + 1, yPos, @intFromFloat((width * progress) - 1), 5, rl.BLACK);
 }
 
 fn UpdateConstructor(dt: f32, entity: *Entity) !void {
@@ -72,6 +101,6 @@ fn UpdateConstructor(dt: f32, entity: *Entity) !void {
             }
         }
     } else {
-        print("Output full\n", .{});
+        // print("Output full\n", .{});
     }
 }
--- graph-testing/src/main.zig
+++ graph-testing/src/main.zig
@@ -6,40 +6,46 @@ const Recipe = @import("recipe.zig").Recipe;
 const ItemPrototype = @import("recipe.zig").ItemPrototype;
 const Entity = @import("entity.zig").Entity;
 const EntityKind = @import("entity.zig").EntityKind;
+const UiData = @import("entity.zig").UiData;
 const CreateConstructor = @import("entity.zig").CreateConstructor;
 
 const print = std.debug.print;
 
 const ArrayList = std.ArrayList;
 
-const Game = struct {
+const World = struct {
     allocator: std.mem.Allocator,
     recipes: ArrayList(Recipe),
     itemPrototypes: ArrayList(ItemPrototype),
     entities: ArrayList(Entity),
-    pub fn init(allocator: std.mem.Allocator) !Game {
-        return Game{ .allocator = allocator, .itemPrototypes = try ArrayList(ItemPrototype).initCapacity(allocator, 0), .recipes = try ArrayList(Recipe).initCapacity(allocator, 0), .entities = try ArrayList(Entity).initCapacity(allocator, 0) };
+    pub fn init(allocator: std.mem.Allocator) !World {
+        return World{ .allocator = allocator, .itemPrototypes = try ArrayList(ItemPrototype).initCapacity(allocator, 0), .recipes = try ArrayList(Recipe).initCapacity(allocator, 0), .entities = try ArrayList(Entity).initCapacity(allocator, 0) };
     }
-    pub fn deinit(self: *Game) void {
+    pub fn deinit(self: *World) void {
         self.itemPrototypes.deinit(self.allocator);
         self.recipes.deinit(self.allocator);
         self.entities.deinit(self.allocator);
     }
 
-    pub fn update(self: *Game) !void {
+    pub fn update(self: *World) !void {
         const dt = rl.GetFrameTime();
         for (self.entities.items) |*entity| {
             try entity.update(dt);
         }
     }
 
-    pub fn addEntity(self: *Game, kind: EntityKind) !void {
+    pub fn draw(self: World) !void {
+        for (self.entities.items) |*entity| {
+            try entity.draw();
+        }
+    }
+    pub fn addEntity(self: *World, kind: EntityKind, uiData: ?UiData) !void {
         const nextId = self.entities.items.len;
-        const entity = try Entity.init(nextId, kind);
+        const entity = try Entity.init(self.allocator, nextId, kind, uiData);
         try self.entities.append(self.allocator, entity);
     }
 
-    pub fn addRecipe(self: *Game, name: []const u8, cost: f32, outputName: []const u8) !void {
+    pub fn addRecipe(self: *World, name: []const u8, cost: f32, outputName: []const u8) !void {
         const item = self.getItemPrototypeByName(outputName);
         if (item) |i| {
             const recipe = try Recipe.init(self.allocator, name, cost, i);
@@ -48,7 +54,7 @@ const Game = struct {
             return error.ItemNotFound;
         }
     }
-    pub fn getRecipeByName(self: *Game, name: []const u8) ?*Recipe {
+    pub fn getRecipeByName(self: *World, name: []const u8) ?*Recipe {
         for (self.recipes.items) |*recipe| {
             if (std.mem.eql(u8, recipe.name, name)) {
                 return recipe; // return pointer to the matching prototype
@@ -56,11 +62,11 @@ const Game = struct {
         }
         return null; // not found
     }
-    pub fn addItemPrototype(self: *Game, input: []const u8) !void {
+    pub fn addItemPrototype(self: *World, input: []const u8) !void {
         const proto = try ItemPrototype.init(self.allocator, input);
         try self.itemPrototypes.append(self.allocator, proto);
     }
-    pub fn getItemPrototypeByName(self: *Game, name: []const u8) ?*ItemPrototype {
+    pub fn getItemPrototypeByName(self: *World, name: []const u8) ?*ItemPrototype {
         for (self.itemPrototypes.items) |*proto| {
             if (std.mem.eql(u8, proto.name, name)) {
                 return proto; // return pointer to the matching prototype
@@ -73,38 +79,34 @@ const Game = struct {
 pub fn main() !void {
     rl.InitWindow(800, 600, "Hello Zig + Raylib");
     const allocator = std.heap.page_allocator;
-    var game = Game.init(allocator) catch |err| {
-        print("Failed to init Game: {}\n", .{err});
+    var world = World.init(allocator) catch |err| {
+        print("Failed to init World: {}\n", .{err});
         return; // exit main gracefully
     };
-    defer game.deinit();
-    // const miner = Entity{ .id = 1, .type = EntityType.Constructor, .uiData = .{
-    //     .rectangle = .{ .x = 20, .y = 20, .width = 100, .height = 100 },
-    // } };
-    //
-
-    try game.addItemPrototype("iron-ore");
-    const proto = game.getItemPrototypeByName("iron-ore");
+    defer world.deinit();
+    try world.addItemPrototype("iron-ore");
+    const proto = world.getItemPrototypeByName("iron-ore");
     if (proto) |p| {
         print("{f}\n", .{p.*});
     } else {
         print("ItemPrototype = null\n", .{});
     }
 
-    try game.addRecipe("mine-iron-ore", 1, "iron-ore");
-    const r = game.getRecipeByName("mine-iron-ore");
+    try world.addRecipe("mine-iron-ore", 1, "iron-ore");
+    const r = world.getRecipeByName("mine-iron-ore");
     if (r) |res| {
-        print("{f}\n", .{res.*});
-        try game.addEntity(try CreateConstructor(allocator, res));
+        //print("{f}\n", .{res.*});
+        try world.addEntity(try CreateConstructor(allocator, res), .{ .rectangle = .{ .x = 100, .y = 100, .width = 200, .height = 100 } });
     } else {
         print("Recipe = null\n", .{});
     }
 
     while (!rl.WindowShouldClose()) {
-        try game.update();
+        try world.update();
         rl.BeginDrawing();
-        rl.ClearBackground(rl.RAYWHITE);
-        rl.DrawText("Hello from Zig on Windows!", 190, 200, 20, rl.LIGHTGRAY);
+        rl.ClearBackground(rl.DARKGRAY);
+        try world.draw();
+        //rl.DrawText("Hello from Zig on Windows!", 190, 200, 20, rl.LIGHTGRAY);
 
         // miner.draw();
         rl.EndDrawing();

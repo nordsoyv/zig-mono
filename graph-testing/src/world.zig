a158c68827eb1dbf043d192efc709adb0aaabd91
Øyvind Nordsveen <oyvind.nordsveen@gmail.com>
2025-11-22 18:59:58 +0100

    -

--- graph-testing/src/world.zig
+++ graph-testing/src/world.zig
@@ -18,67 +18,77 @@ pub fn getWorld(allocator: std.mem.Allocator) !*World {
 
 pub const World = struct {
     allocator: std.mem.Allocator,
-    recipes: ArrayList(Recipe),
-    itemPrototypes: ArrayList(ItemPrototype),
-    entities: ArrayList(Entity),
+    recipes: std.StringHashMap(Recipe),
+    itemPrototypes: std.StringHashMap(ItemPrototype),
+    entities: std.AutoHashMap(usize, Entity),
+    nextEntityId: usize,
     pub fn init(allocator: std.mem.Allocator) !World {
-        return World{ .allocator = allocator, .itemPrototypes = try ArrayList(ItemPrototype).initCapacity(allocator, 0), .recipes = try ArrayList(Recipe).initCapacity(allocator, 0), .entities = try ArrayList(Entity).initCapacity(allocator, 0) };
+        return World{
+            .allocator = allocator,
+            .recipes = std.StringHashMap(Recipe).init(allocator),
+            .itemPrototypes = std.StringHashMap(ItemPrototype).init(allocator),
+            .entities = std.AutoHashMap(usize, Entity).init(allocator),
+            .nextEntityId = 0,
+        };
     }
     pub fn deinit(self: *World) void {
-        self.itemPrototypes.deinit(self.allocator);
-        self.recipes.deinit(self.allocator);
-        self.entities.deinit(self.allocator);
+        self.itemPrototypes.deinit();
+        self.recipes.deinit();
+        self.entities.deinit();
     }
 
     pub fn update(self: *World) !void {
         const dt = rl.getFrameTime();
-        // var it = self.entities.iterator();
-        // while (it.next()) |*entity| {
-        //     try entity.update(dt);
-        // }
-        for (self.entities.items) |*entity| {
-            try entity.update(dt);
+        var it = self.entities.iterator();
+        while (it.next()) |*entry| {
+            try entry.value_ptr.*.update(dt);
         }
     }
 
     pub fn draw(self: World) !void {
-        for (self.entities.items) |*entity| {
-            try entity.draw();
+        var it = self.entities.iterator();
+        while (it.next()) |entry| {
+            try entry.value_ptr.*.draw();
         }
     }
     pub fn addEntity(self: *World, kind: EntityKind, uiData: ?UiData) !void {
-        const nextId = self.entities.items.len;
+        const nextId = self.nextEntityId;
+        self.nextEntityId += 1;
         const entity = try Entity.init(self.allocator, nextId, kind, uiData);
-        try self.entities.append(self.allocator, entity);
+        try self.entities.put(nextId, entity);
+    }
+
+    pub fn getEntity(self: *World, id: usize) *Entity {
+        if (self.entities.contains(id)) {
+            return self.entities.get(id).*;
+        } else {
+            return null;
+        }
     }
 
     pub fn addRecipe(self: *World, name: []const u8, cost: f32, outputName: []const u8) !void {
         const item = self.getItemPrototypeByName(outputName);
         if (item) |i| {
             const recipe = try Recipe.init(self.allocator, name, cost, i);
-            try self.recipes.append(self.allocator, recipe);
+            try self.recipes.put(name, recipe);
         } else {
             return error.ItemNotFound;
         }
     }
     pub fn getRecipeByName(self: *World, name: []const u8) ?*Recipe {
-        for (self.recipes.items) |*recipe| {
-            if (std.mem.eql(u8, recipe.name, name)) {
-                return recipe; // return pointer to the matching prototype
-            }
+        if (self.recipes.contains(name)) {
+            return self.recipes.getPtr(name);
         }
-        return null; // not found
+        return null;
     }
     pub fn addItemPrototype(self: *World, input: []const u8) !void {
         const proto = try ItemPrototype.init(self.allocator, input);
-        try self.itemPrototypes.append(self.allocator, proto);
+        try self.itemPrototypes.put(input, proto);
     }
     pub fn getItemPrototypeByName(self: *World, name: []const u8) ?*ItemPrototype {
-        for (self.itemPrototypes.items) |*proto| {
-            if (std.mem.eql(u8, proto.name, name)) {
-                return proto; // return pointer to the matching prototype
-            }
+        if (self.itemPrototypes.contains(name)) {
+            return self.itemPrototypes.getPtr(name);
         }
-        return null; // not found
+        return null;
     }
 };
